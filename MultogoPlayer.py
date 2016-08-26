a0aa80b1c57e68ee1135a65b9808fb7f6a264ac2
Killer Chief <KILLER_CHIEF@hotmail.com>
2016-08-27 01:08:43 +1000

    Added Player Class Object

---
+++ MultogoPlayer.py
@@ -0,0 +1,47 @@
+
+
+class Player(object):
+	#
+	
+	def __init__(self, symbol, wipePlayersOnLose):
+		self.symbol = symbol
+		self.ai = False
+		self.lost = False
+		self.wipedOffBoard = wipePlayersOnLose
+		self.moveCount = 0
+		self.kills = 0
+		self.stringKills = 0
+	
+	def getSymbol(self):
+		return self.symbol
+	
+	def isAi(self, ai):
+		return self.ai
+	
+	def setAi(self, ai):
+		self.ai = ai
+	
+	def hasLost(self):
+		return self.lost
+	
+	def setLost(self):
+		self.lost = True
+	
+	def isWipedOffBoard(self):
+		return self.wipedOffBoard
+	
+	def setWipedOffBoard(self):
+		self.wipedOffBoard = True
+	
+	def incrementMoveCount(self):
+		self.moveCount += 1
+	
+	def incrementKills(self):
+		self.kills += 1
+	
+	def incrementStringKills(self):
+		self.stringKills += 1
+	
+	
+if __name__ == '__main__':
+	print "test compile success?"
