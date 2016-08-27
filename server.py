f0b23b3b0adaa02e609e7741747ebc9ef5a6de34
mcqueen256 <bucknich@gmail.com>
2016-08-27 15:59:46 +1000

    Simplified

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ from tornado.websocket import WebSocketHandler
 
 define("port", default=8888, help="run on the given port", type=int)
 
-class EchoWebSocket(WebSocketHandler):
+class PlayerWebSocket(WebSocketHandler):
     def open(self):
         print("WebSocket opened")
 
@@ -40,76 +40,26 @@ class EchoWebSocket(WebSocketHandler):
     def on_close(self):
         print("WebSocket closed")
 
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        return self.get_secure_cookie("user")
-
-class MainHandler(BaseHandler):
-    @tornado.web.authenticated
+class LobbyPageHandler(RequestHandler):
     def get(self):
         self.render('index.html')
 
-class LoginHandler(BaseHandler):
-    @tornado.gen.coroutine
-    def get(self):
-        incorrect = self.get_secure_cookie("incorrect")
-        if incorrect and int(incorrect) > 20:
-            self.write('<center>blocked</center>')
-            return
-        self.render('login.html')
-
-    @tornado.gen.coroutine
-    def post(self):
-        incorrect = self.get_secure_cookie("incorrect")
-        if incorrect and int(incorrect) > 20:
-            self.write('<center>blocked</center>')
-            return
-
-        # TODO: other then demo?
-        getusername = tornado.escape.xhtml_escape(self.get_argument("username"))
-        getpassword = tornado.escape.xhtml_escape(self.get_argument("password"))
-        if "demo" == getusername and "demo" == getpassword:
-            self.set_secure_cookie("user", self.get_argument("username"))
-            self.set_secure_cookie("incorrect", "0")
-            self.redirect(self.reverse_url("main"))
-        else:
-            incorrect = self.get_secure_cookie("incorrect") or 0
-            increased = str(int(incorrect)+1)
-            self.set_secure_cookie("incorrect", increased)
-            self.write("""<center>
-                            Something Wrong With Your Data (%s)<br />
-                            <a href="/">Go Home</a>
-                          </center>""" % increased)
-
-
-class LogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("user")
-        self.redirect(self.get_argument("next", self.reverse_url("main")))
-
-class Application(tornado.web.Application):
+class App(Application):
     def __init__(self):
-        base_dir = os.path.dirname(__file__)
-        settings = {
-            "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
-            "login_url": "/login",
-            'template_path': os.path.join(base_dir, "templates"),
-            'static_path': os.path.join(base_dir, "static"),
-            'debug':True,
-            "xsrf_cookies": True,
-        }
-
+        settings = {}
         tornado.web.Application.__init__(self, [
-            tornado.web.url(r"/", MainHandler, name="main"),
-            tornado.web.url(r'/login', LoginHandler, name="login"),
-            tornado.web.url(r'/logout', LogoutHandler, name="logout"),
-            tornado.web.url(r'/websocket', EchoWebSocket, name="ws"),
+            tornado.web.url(r"/", LobbyPageHandler, name="lobby"),
+            tornado.web.url(r'/websocket', PlayerWebSocket, name="ws"),
         ], **settings)
+
+        #self.lobbyHandler = LobbyHandler(self)
+        self.gameSessions = {}
+        self.playerHandlers = {}
 
 def main():
     tornado.art.show()
     options.parse_command_line()
-    Application().listen(options.port)
+    App().listen(options.port)
     IOLoop.instance().start()
 
 if __name__ == "__main__":
