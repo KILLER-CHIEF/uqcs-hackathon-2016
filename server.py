4ec268ea9911e0d92a115a8312aff2063eabc8f6
mcqueen256 <bucknich@gmail.com>
2016-08-27 16:12:33 +1000

    updates to server

--- server.py
+++ server.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
 
 import logging, os
 
@@ -30,7 +16,12 @@ from tornado.websocket import WebSocketHandler
 
 define("port", default=8888, help="run on the given port", type=int)
 
-class PlayerWebSocket(WebSocketHandler):
+class PlayerHandler(WebSocketHandler):
+    def __init__(self, *argv, **argkw):
+        super().__init__(self, *args, **kwargs)
+        
+        return
+
     def open(self):
         print("WebSocket opened")
 
@@ -49,7 +40,7 @@ class App(Application):
         settings = {}
         tornado.web.Application.__init__(self, [
             tornado.web.url(r"/", LobbyPageHandler, name="lobby"),
-            tornado.web.url(r'/websocket', PlayerWebSocket, name="ws"),
+            tornado.web.url(r'/websocket', PlayerHandler, name="ws"),
         ], **settings)
 
         #self.lobbyHandler = LobbyHandler(self)
