9f80100ffa606c102f76b814a338bb76f9d6077c
James <james.bentley@uq.net.au>
2016-08-27 16:11:43 +1000

    Added lobby page

---
+++ templates/js/lobby.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+
+    function onRowClick(tableId, callback) {
+    var table = document.getElementById(tableId),
+        rows = table.getElementsByTagName("tr"),
+        i;
+    for (i = 0; i < rows.length; i++) {
+        table.rows[i].onclick = function (row) {
+            return function () {
+                callback(row);
+            };
+        }(table.rows[i]);
+    }
+};
+ 
+onRowClick("games", function (row){
+    var value = row.getElementsByTagName("td")[0].innerHTML;
+    //document.getElementById('click-response').innerHTML = value + " clicked!";
+    console.log("value>>", value);
+});
+    
+}
+
