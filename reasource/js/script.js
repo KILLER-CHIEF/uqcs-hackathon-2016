c4080164caac4064d428ef4b85b87a5971327846
James <james.bentley@uq.net.au>
2016-08-28 15:48:27 +1000

    sds

--- reasource/js/script.js
+++ reasource/js/script.js
@@ -45,6 +45,7 @@ function create_board(data) {
         for (var j = 0 - 1; j <= width; i++) {
             var td = $('td')
             tr.add(td);
+            td.text(board_content[i*width+j]);
             td.attr("id","row"+i+"col"+j);
             
         }
@@ -78,40 +79,5 @@ function get_board() {
 
 window.onload = function(){
     create_board();
-var width =800;
-var height = 800;
-var gridWidth = 8;
-var gridHeight = 16;
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-canvas.addEventListener('click', handleClick);
-    function handleClick(e) {
-        ctx.font="50px Verdana";
-		ctx.fillText("A",Math.floor(e.offsetX/(width/gridWidth))*(width/gridWidth),(Math.floor(e.offsetY/(height/gridHeight))*(height/gridHeight))+(height/gridHeight),(width/gridWidth));
-      
-        //ctx.fillRect(Math.floor(e.offsetX/(width/gridWidth))*(width/gridWidth), 
-                  // Math.floor(e.offsetY/(height/gridHeight))*(height/gridHeight),
-                  //(width/gridWidth), (height/gridHeight));
-        console.log(Math.floor(e.offsetX/(width/gridWidth)));
-        console.log(Math.floor(e.offsetY/(height/gridHeight)));
-        //console.log(e.offsetY);
-	}
-function drawBoard(){
-for (var x = 0; x <= width; x += (width/gridWidth)) {
-    ctx.moveTo(x,0);
-    ctx.lineTo( x, height);
-}
-
-
-for (var x = 0; x <= height; x += (height/gridHeight)) {
-    ctx.moveTo(0, x);
-    ctx.lineTo(width,x);
-}
-
-ctx.strokeStyle = "black";
-ctx.stroke();
-}
-
-drawBoard();
 }
