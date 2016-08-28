190ce9dc9e645cc38bd127032a29845b80d860c9
James <james.bentley@uq.net.au>
2016-08-28 12:27:35 +1000

    changes to js file

--- reasource/js/script.js
+++ reasource/js/script.js
@@ -1,6 +1,8 @@
 window.onload = function(){
 var width =800;
 var height = 800;
+var gridWidth = 16;
+var gridHeight = 24;
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -8,18 +10,20 @@ canvas.addEventListener('click', handleClick);
     function handleClick(e) {
 		ctx.fillStyle = "black";
       
-        ctx.fillRect(Math.floor(e.offsetX/180)*200, 
-                   Math.floor(e.offsetY/180)*200,
-                   200, 200);
+        ctx.fillRect(Math.floor(e.offsetX/(width/gridWidth))*(width/gridWidth), 
+                   Math.floor(e.offsetY/(height/gridHeight))*(height/gridHeight),
+                  (width/gridWidth), (height/gridHeight));
+        console.log(e.offsetX);
+        console.log(e.offsetY);
 	}
 function drawBoard(){
-for (var x = 0; x <= width; x += 200) {
+for (var x = 0; x <= width; x += (width/gridWidth)) {
     ctx.moveTo(x,0);
     ctx.lineTo( x, height);
 }
 
 
-for (var x = 0; x <= height; x += 200) {
+for (var x = 0; x <= height; x += (height/gridHeight)) {
     ctx.moveTo(0, x);
     ctx.lineTo(width,x);
 }
