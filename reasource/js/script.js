d80f413e036c5651562e489c60d0dfb42f442798
mcqueen256 <bucknich@gmail.com>
2016-08-28 15:48:45 +1000

    ashhhhhhhh

--- reasource/js/script.js
+++ reasource/js/script.js
@@ -36,9 +36,14 @@ function create_board(data) {
     var board_data = data.split(',');
     var width = board_data[0];
     var height = board_data[1];
+    var board_content = board_data[2];
     var board = $('#board');
 
+    // handle table deconstruction
+    if ()
+
     var table = $('table');
+    table.attr('id', 'game-table');
     for (var i = 0 - 1; i <= height; i++) {
         var tr = $('tr');
         table.add(tr);
@@ -77,41 +82,5 @@ function get_board() {
 
 
 window.onload = function(){
-    create_board();
-var width =800;
-var height = 800;
-var gridWidth = 8;
-var gridHeight = 16;
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-canvas.addEventListener('click', handleClick);
-    function handleClick(e) {
-        ctx.font="50px Verdana";
-		ctx.fillText("A",Math.floor(e.offsetX/(width/gridWidth))*(width/gridWidth),(Math.floor(e.offsetY/(height/gridHeight))*(height/gridHeight))+(height/gridHeight),(width/gridWidth));
-      
-        //ctx.fillRect(Math.floor(e.offsetX/(width/gridWidth))*(width/gridWidth), 
-                  // Math.floor(e.offsetY/(height/gridHeight))*(height/gridHeight),
-                  //(width/gridWidth), (height/gridHeight));
-        console.log(Math.floor(e.offsetX/(width/gridWidth)));
-        console.log(Math.floor(e.offsetY/(height/gridHeight)));
-        //console.log(e.offsetY);
-	}
-function drawBoard(){
-for (var x = 0; x <= width; x += (width/gridWidth)) {
-    ctx.moveTo(x,0);
-    ctx.lineTo( x, height);
-}
-
-
-for (var x = 0; x <= height; x += (height/gridHeight)) {
-    ctx.moveTo(0, x);
-    ctx.lineTo(width,x);
-}
-
-ctx.strokeStyle = "black";
-ctx.stroke();
-}
-
-drawBoard();
 }
